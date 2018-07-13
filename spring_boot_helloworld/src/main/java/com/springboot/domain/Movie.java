bf93345f596d9202c7844b1dbef34ca3bf9c06d5
wenjie <wenjie@xiaomi.com>
2018-07-13 20:23:32 +0800

    fix

---
+++ spring_boot_helloworld/src/main/java/com/springboot/domain/Movie.java
@@ -0,0 +1,68 @@
+package com.springboot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String video_id;
+	private String start;
+	private String language;
+	private String director;
+	private String title;
+	private String location;
+	private String url;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getVideo_id() {
+		return video_id;
+	}
+	public void setVideo_id(String video_id) {
+		this.video_id = video_id;
+	}
+	public String getStart() {
+		return start;
+	}
+	public void setStart(String start) {
+		this.start = start;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
